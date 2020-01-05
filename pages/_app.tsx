bb6e135cfabba7532f9233274cfdd5b22d9620dc
Brett Dorrans <hello@lapidist.net>
2020-01-05 19:57:30 +0000

    fix: path to static assets

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -32,24 +32,24 @@ export default class MyApp extends App {
                     <link
                         rel="apple-touch-icon"
                         sizes="180x180"
-                        href="/apple-touch-icon.png"
+                        href="apple-touch-icon.png"
                     />
                     <link
                         rel="icon"
                         type="image/png"
                         sizes="32x32"
-                        href="/favicon-32x32.png"
+                        href="favicon-32x32.png"
                     />
                     <link
                         rel="icon"
                         type="image/png"
                         sizes="16x16"
-                        href="/favicon-16x16.png"
+                        href="favicon-16x16.png"
                     />
-                    <link rel="manifest" href="/manifest.json" />
+                    <link rel="manifest" href="manifest.json" />
                     <link
                         rel="mask-icon"
-                        href="/safari-pinned-tab.svg"
+                        href="safari-pinned-tab.svg"
                         color="#5bbad5"
                     />
                     <link rel="canonical" href="https://lapidist.net/" />
