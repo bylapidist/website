ae35e58f419a8b1e7d7d20760fff415bf577c4c3
Brett Dorrans <hello@lapidist.net>
2020-01-05 22:34:15 +0000

    feat: analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,20 @@ export default class MyApp extends App {
         return (
             <>
                 <Head>
+                    <script
+                        async
+                        src="https://www.googletagmanager.com/gtag/js?id=UA-48222807-1"
+                    />
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'UA-48222807-1');
+                    `
+                        }}
+                    />
                     <title>
                         Brett Dorrans: Software Engineer in Glasgow, Scotland
                     </title>
