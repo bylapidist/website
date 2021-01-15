cc146048c415077715e1cf3032faa3cf45d1c8bd
Brett Dorrans <hello@lapidist.net>
2021-01-15 18:29:41 +0000

    refactor: use PanelHeading component (#823)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,8 @@ import {
     Link,
     Logo,
     Text,
-    Panel
+    Panel,
+    PanelHeading
 } from '@lapidist/components';
 import IndexLayout from '../layouts';
 import {
@@ -18,10 +19,7 @@ import {
     introStyles,
     linkContainerStyles,
     linkStyles,
-    logoStyles,
-    cardTertiaryHeadingStyles,
-    cardSecondaryHeadingStyles,
-    cardPrimaryHeadingStyles
+    logoStyles
 } from '../styles';
 
 const IndexPage = (): JSX.Element => (
@@ -61,170 +59,164 @@ const IndexPage = (): JSX.Element => (
             </Text>
             <Flex styles={cardContainerStyles}>
                 <Panel kind="tertiary" styles={cardStyles}>
-                    <Heading size={3} styles={cardTertiaryHeadingStyles}>
+                    <PanelHeading kind="tertiary" size={3}>
                         components
-                    </Heading>
-                    <Box>
-                        <Text styles={cardDescriptionStyles}>
-                            Opinionated{' '}
+                    </PanelHeading>
+                    <Text styles={cardDescriptionStyles}>
+                        Opinionated{' '}
+                        <Link
+                            href="https://reactjs.org/"
+                            target="_blank"
+                            rel="nofollow noopener"
+                        >
+                            React
+                        </Link>{' '}
+                        UI library, built with{' '}
+                        <Link
+                            href="https://styled-components.com/"
+                            target="_blank"
+                            rel="nofollow noopener"
+                        >
+                            styled-components
+                        </Link>{' '}
+                        and{' '}
+                        <Link
+                            href="https://www.typescriptlang.org/"
+                            target="_blank"
+                            rel="nofollow noopener"
+                        >
+                            TypeScript
+                        </Link>
+                        .
+                    </Text>
+                    <Flex styles={{ paddingY: '4' }}>
+                        <Box styles={{ marginRight: '2' }}>
                             <Link
-                                href="https://reactjs.org/"
-                                target="_blank"
+                                href="https://www.npmjs.com/package/@lapidist/components"
                                 rel="nofollow noopener"
                             >
-                                React
-                            </Link>{' '}
-                            UI library, built with{' '}
-                            <Link
-                                href="https://styled-components.com/"
-                                target="_blank"
-                                rel="nofollow noopener"
-                            >
-                                styled-components
-                            </Link>{' '}
-                            and{' '}
-                            <Link
-                                href="https://www.typescriptlang.org/"
-                                target="_blank"
-                                rel="nofollow noopener"
-                            >
-                                TypeScript
+                                <img
+                                    src="https://img.shields.io/npm/v/@lapidist/components"
+                                    alt="@lapidist/components npm version"
+                                />
                             </Link>
-                            .
-                        </Text>
-                        <Flex styles={{ paddingY: '4' }}>
-                            <Box styles={{ marginRight: '2' }}>
-                                <Link
-                                    href="https://www.npmjs.com/package/@lapidist/components"
-                                    rel="nofollow noopener"
-                                >
-                                    <img
-                                        src="https://img.shields.io/npm/v/@lapidist/components"
-                                        alt="@lapidist/components npm version"
-                                    />
-                                </Link>
-                            </Box>
-                            <Box>
-                                <Link
-                                    href="https://github.com/bylapidist/components"
-                                    rel="nofollow noopener"
-                                >
-                                    <img
-                                        src="https://github.com/bylapidist/components/workflows/Release/badge.svg"
-                                        alt="@lapidist/components build status"
-                                    />
-                                </Link>
-                            </Box>
-                        </Flex>
-                    </Box>
+                        </Box>
+                        <Box>
+                            <Link
+                                href="https://github.com/bylapidist/components"
+                                rel="nofollow noopener"
+                            >
+                                <img
+                                    src="https://github.com/bylapidist/components/workflows/Release/badge.svg"
+                                    alt="@lapidist/components build status"
+                                />
+                            </Link>
+                        </Box>
+                    </Flex>
                 </Panel>
                 <Panel kind="secondary" styles={cardStyles}>
-                    <Heading size={3} styles={cardSecondaryHeadingStyles}>
+                    <PanelHeading kind="secondary" size={3}>
                         styles
-                    </Heading>
-                    <Box>
-                        <Text styles={cardDescriptionStyles}>
-                            Strongly typed theming framework for{' '}
+                    </PanelHeading>
+                    <Text styles={cardDescriptionStyles}>
+                        Strongly typed theming framework for{' '}
+                        <Link
+                            href="https://reactjs.org/"
+                            target="_blank"
+                            rel="nofollow noopener"
+                        >
+                            React
+                        </Link>{' '}
+                        components built with{' '}
+                        <Link
+                            href="https://github.com/frenic/csstype"
+                            target="_blank"
+                            rel="nofollow noopener"
+                        >
+                            CSSType
+                        </Link>
+                        , inspired by the{' '}
+                        <Link
+                            href="https://tailwindcss.com/"
+                            target="_blank"
+                            rel="nofollow noopener"
+                        >
+                            tailwindcss
+                        </Link>{' '}
+                        API.
+                    </Text>
+                    <Flex styles={{ paddingY: '4' }}>
+                        <Box styles={{ marginRight: '2' }}>
                             <Link
-                                href="https://reactjs.org/"
-                                target="_blank"
+                                href="https://www.npmjs.com/package/@lapidist/styles"
                                 rel="nofollow noopener"
                             >
-                                React
-                            </Link>{' '}
-                            components built with{' '}
-                            <Link
-                                href="https://github.com/frenic/csstype"
-                                target="_blank"
-                                rel="nofollow noopener"
-                            >
-                                CSSType
+                                <img
+                                    src="https://img.shields.io/npm/v/@lapidist/styles"
+                                    alt="@lapidist/styles npm version"
+                                />
                             </Link>
-                            , inspired by the{' '}
+                        </Box>
+                        <Box>
                             <Link
-                                href="https://tailwindcss.com/"
-                                target="_blank"
+                                href="https://github.com/bylapidist/styles"
                                 rel="nofollow noopener"
                             >
-                                tailwindcss
-                            </Link>{' '}
-                            API.
-                        </Text>
-                        <Flex styles={{ paddingY: '4' }}>
-                            <Box styles={{ marginRight: '2' }}>
-                                <Link
-                                    href="https://www.npmjs.com/package/@lapidist/styles"
-                                    rel="nofollow noopener"
-                                >
-                                    <img
-                                        src="https://img.shields.io/npm/v/@lapidist/styles"
-                                        alt="@lapidist/styles npm version"
-                                    />
-                                </Link>
-                            </Box>
-                            <Box>
-                                <Link
-                                    href="https://github.com/bylapidist/styles"
-                                    rel="nofollow noopener"
-                                >
-                                    <img
-                                        src="https://github.com/bylapidist/styles/workflows/Release/badge.svg"
-                                        alt="@lapidist/styles build status"
-                                    />
-                                </Link>
-                            </Box>
-                        </Flex>
-                    </Box>
+                                <img
+                                    src="https://github.com/bylapidist/styles/workflows/Release/badge.svg"
+                                    alt="@lapidist/styles build status"
+                                />
+                            </Link>
+                        </Box>
+                    </Flex>
                 </Panel>
                 <Panel kind="primary" styles={cardStyles}>
-                    <Heading size={3} styles={cardPrimaryHeadingStyles}>
+                    <PanelHeading kind="primary" size={3}>
                         linting
-                    </Heading>
-                    <Box>
-                        <Text styles={cardDescriptionStyles}>
-                            My personal{' '}
+                    </PanelHeading>
+                    <Text styles={cardDescriptionStyles}>
+                        My personal{' '}
+                        <Link
+                            href="https://eslint.org/"
+                            target="_blank"
+                            rel="nofollow noopener"
+                        >
+                            ESlint
+                        </Link>{' '}
+                        and{' '}
+                        <Link
+                            href="https://prettier.io/"
+                            target="_blank"
+                            rel="nofollow noopener"
+                        >
+                            Prettier
+                        </Link>{' '}
+                        configurations.
+                    </Text>
+                    <Flex styles={{ paddingY: '4' }}>
+                        <Box styles={{ marginRight: '2' }}>
                             <Link
-                                href="https://eslint.org/"
-                                target="_blank"
+                                href="https://www.npmjs.com/package/@lapidist/linting"
                                 rel="nofollow noopener"
                             >
-                                ESlint
-                            </Link>{' '}
-                            and{' '}
+                                <img
+                                    src="https://img.shields.io/npm/v/@lapidist/linting"
+                                    alt="@lapidist/linting npm version"
+                                />
+                            </Link>
+                        </Box>
+                        <Box>
                             <Link
-                                href="https://prettier.io/"
-                                target="_blank"
+                                href="https://github.com/bylapidist/linting"
                                 rel="nofollow noopener"
                             >
-                                Prettier
-                            </Link>{' '}
-                            configurations.
-                        </Text>
-                        <Flex styles={{ paddingY: '4' }}>
-                            <Box styles={{ marginRight: '2' }}>
-                                <Link
-                                    href="https://www.npmjs.com/package/@lapidist/linting"
-                                    rel="nofollow noopener"
-                                >
-                                    <img
-                                        src="https://img.shields.io/npm/v/@lapidist/linting"
-                                        alt="@lapidist/linting npm version"
-                                    />
-                                </Link>
-                            </Box>
-                            <Box>
-                                <Link
-                                    href="https://github.com/bylapidist/linting"
-                                    rel="nofollow noopener"
-                                >
-                                    <img
-                                        src="https://github.com/bylapidist/linting/workflows/Release/badge.svg"
-                                        alt="@lapidist/linting build status"
-                                    />
-                                </Link>
-                            </Box>
-                        </Flex>
-                    </Box>
+                                <img
+                                    src="https://github.com/bylapidist/linting/workflows/Release/badge.svg"
+                                    alt="@lapidist/linting build status"
+                                />
+                            </Link>
+                        </Box>
+                    </Flex>
                 </Panel>
             </Flex>
         </Box>
