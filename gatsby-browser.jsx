2a38145215954360adda9f60f3ff3c512d1bd7ff
Brett Dorrans <hello@lapidist.net>
2022-08-23 16:41:12 +0100

    feat: use components@7 (#1442)

--- gatsby-browser.jsx
+++ gatsby-browser.jsx
@@ -1,29 +1,10 @@
 const React = require('react');
-const {
-    mergeThemes,
-    defaultTheme,
-    darkTheme,
-    ThemeProvider
-} = require('@lapidist/components');
-
-const prefersDarkTheme =
-    (typeof window !== 'undefined' &&
-        window?.matchMedia &&
-        window.matchMedia('(prefers-color-scheme: dark)').matches) ||
-    (typeof localStorage !== 'undefined' &&
-        localStorage.getItem('isDarkMode') === 'true');
+const { defaultTheme, ThemeProvider } = require('@lapidist/components');
 
 // Wraps every page in a component
 exports.wrapPageElement = ({ element, props }) => {
     return (
-        <ThemeProvider
-            {...props}
-            theme={
-                prefersDarkTheme
-                    ? mergeThemes(defaultTheme, darkTheme)
-                    : defaultTheme
-            }
-        >
+        <ThemeProvider {...props} theme={defaultTheme}>
             {element}
         </ThemeProvider>
     );
