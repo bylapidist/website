b228dd74786bf9e899c147cbad238a43967456f8
brettdorrans <hello@lapidist.net>
2023-07-01 13:20:03 +0000

    Deploying to gh-pages from @ bylapidist/website@6e99ffb0fdfe47a79886d33b0ed47fe2378b6efc 🚀

--- sw.js
+++ sw.js
@@ -95,7 +95,7 @@ self.__precacheManifest = [
   },
   {
     "url": "brett-dorrans-cv.pdf",
-    "revision": "5a23f8729583dd7364b7f3a676054bae"
+    "revision": "3fee73a64d78a38c0eecea1f3db7cf97"
   },
   {
     "url": "chunk-map.json",
@@ -174,7 +174,7 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "dda39109f4125b95f6ca29a552fd0ba4"
+    "revision": "b0b1ddca93318a9ebde87106f9372135"
   },
   {
     "url": "logo.svg",
@@ -218,7 +218,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/sq/d/3530040381.json",
-    "revision": "e75240d6cf22c51318f0ab90fb439c59"
+    "revision": "94dbe966338280de0bc6ab47c2c06252"
   },
   {
     "url": "sitemap-0.xml",
