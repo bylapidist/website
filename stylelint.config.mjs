e5b538f21fe7470bd9aab5238e3dcae7a4edcb6f
Brett Dorrans <hello@lapidist.net>
2025-08-11 00:24:39 +0100

    ci: add stylelint (#2313)

---
+++ stylelint.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('stylelint').Config} */
+export default {
+    extends: ["stylelint-config-standard", "stylelint-config-css-modules"],
+    rules: {
+        "color-function-alias-notation": null,
+        "color-hex-length": null,
+        "custom-property-empty-line-before": null,
+        "declaration-block-no-redundant-longhand-properties": null,
+        "media-feature-range-notation": null,
+    },
+};
