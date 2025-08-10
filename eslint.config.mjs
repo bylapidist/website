f0f8dcaa7c31a304ae0116d5a61d50b5a1e84548
Brett Dorrans <hello@lapidist.net>
2025-08-11 00:34:51 +0100

    ci: add storybook (#2314)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import { fileURLToPath } from "node:url";
 import { FlatCompat } from "@eslint/eslintrc";
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
+import storybook from "eslint-plugin-storybook";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -14,6 +15,7 @@ const compat = new FlatCompat({
 export default [
     js.configs.recommended,
     ...tseslint.configs.strictTypeChecked,
+    ...storybook.configs["flat/recommended"],
     ...compat.extends("next/core-web-vitals", "next/typescript"),
     {
         files: ["**/*.{ts,tsx}"],
