f254419335d086816e6eea17fc48aa0c3584376a
Brett Dorrans <hello@lapidist.net>
2025-08-11 21:53:52 +0100

    feat: add company info to footer

--- tests/smoke.spec.ts
+++ tests/smoke.spec.ts
@@ -4,6 +4,9 @@ import AxeBuilder from "@axe-core/playwright";
 test("home renders with basic a11y and performance", async ({ page }) => {
     await page.goto("/");
     await expect(page.locator("h1")).toContainText("design systems");
+    await expect(page.locator("footer")).toContainText(
+        "Company number SC549211",
+    );
     const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
     expect(accessibilityScanResults.violations).toEqual([]);
     const nav = await page.evaluate(
