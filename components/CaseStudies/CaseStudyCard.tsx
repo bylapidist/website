45cdb41e31335905bce500b43a363ef878e0fe27
Brett Dorrans <hello@lapidist.net>
2025-08-13 22:22:12 +0100

    refactor case studies component

---
+++ components/CaseStudies/CaseStudyCard.tsx
@@ -0,0 +1,26 @@
+import Card from "@/components/Card/Card";
+import { ReactNode } from "react";
+
+interface CaseStudyCardProps {
+    title: string;
+    visual: ReactNode;
+    before: string;
+    after: string;
+}
+
+export default function CaseStudyCard({
+    title,
+    visual,
+    before,
+    after,
+}: CaseStudyCardProps) {
+    return (
+        <Card title={title} size="md">
+            {visual}
+            <h4>Before:</h4>
+            <p>{before}</p>
+            <h4>After:</h4>
+            <p>{after}</p>
+        </Card>
+    );
+}
