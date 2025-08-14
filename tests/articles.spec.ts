57c7870fda69829b9e519394a59542e4653cefc5
Brett Dorrans <hello@lapidist.net>
2025-08-14 13:05:26 +0100

    Add custom error pages and tests (#2398)

---
+++ tests/articles.spec.ts
@@ -0,0 +1,22 @@
+import AxeBuilder from "@axe-core/playwright";
+import { expect, test } from "@playwright/test";
+
+test("articles index is accessible", async ({ page }) => {
+    await page.goto("/articles");
+    await expect(page.locator("h1")).toContainText("Articles");
+    const accessibilityScanResults = await new AxeBuilder({ page })
+        .include("main")
+        .exclude('[role="alert"]')
+        .analyze();
+    expect(accessibilityScanResults.violations).toEqual([]);
+});
+
+test("article page is accessible", async ({ page }) => {
+    await page.goto("/2025/on-freedom-curiosity-and-happiness");
+    await expect(page.locator("article")).toBeVisible();
+    const accessibilityScanResults = await new AxeBuilder({ page })
+        .include("main")
+        .exclude('[role="alert"]')
+        .analyze();
+    expect(accessibilityScanResults.violations).toEqual([]);
+});
