5241836b2e367bab2a2ed216be8972792bb68b48
Brett Dorrans <hello@lapidist.net>
2025-08-15 09:26:50 +0100

    Add footer to blog pages (#2404)

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -1,23 +1,29 @@
 import Link from "next/link";
 import Card from "@/components/Card/Card";
+import Contact from "@/components/Contact/Contact";
 import Container from "@/components/Container/Container";
+import Footer from "@/components/Footer/Footer";
 import { getAllArticles } from "@/lib/articles";
 import styles from "./page.module.scss";
 
 export default async function ArticlesPage() {
     const articles = await getAllArticles();
     return (
-        <Container as="section">
-            <h1>Articles</h1>
-            <div className={styles.cards}>
-                {articles.map(({ year, slug, title, description }) => (
-                    <Link key={`${year}-${slug}`} href={`/${year}/${slug}`}>
-                        <Card title={title} headingLevel="h2">
-                            <p>{description}</p>
-                        </Card>
-                    </Link>
-                ))}
-            </div>
-        </Container>
+        <>
+            <Container as="section">
+                <h1>Articles</h1>
+                <div className={styles.cards}>
+                    {articles.map(({ year, slug, title, description }) => (
+                        <Link key={`${year}-${slug}`} href={`/${year}/${slug}`}>
+                            <Card title={title} headingLevel="h2">
+                                <p>{description}</p>
+                            </Card>
+                        </Link>
+                    ))}
+                </div>
+            </Container>
+            <Contact />
+            <Footer />
+        </>
     );
 }
