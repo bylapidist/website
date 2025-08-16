726f98db26a5ad19609df6336119fec34ee807f3
Brett Dorrans <hello@lapidist.net>
2025-08-16 11:13:19 +0100

    Revert "refactor: use css variables in static svgs (#2424)" (#2426)
    This reverts commit 3baa96a2d6df86f62fd2dcd66eb2145016656997.

---
+++ components/ThemeToggle/MoonIcon.tsx
@@ -0,0 +1,9 @@
+import type { SVGProps } from "react";
+
+export default function MoonIcon(props: SVGProps<SVGSVGElement>) {
+    return (
+        <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
+            <path d="M21 12.79A9 9 0 0 1 11.21 3 7 7 0 1 0 21 12.79z" />
+        </svg>
+    );
+}
