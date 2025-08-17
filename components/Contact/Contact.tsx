960398c8d4b910ced33e4bf4b5a2cf29e402a3e2
Brett Dorrans <hello@lapidist.net>
2025-08-17 02:09:32 +0100

    refactor: streamline contact section heading (#2460)

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -4,10 +4,11 @@ import styles from "./Contact.module.scss";
 
 export default function Contact() {
     return (
-        <Section id="contact" labelledBy="contact-heading">
-            <h2 id="contact-heading" className={styles.heading}>
-                Let&apos;s work together
-            </h2>
+        <Section
+            id="contact"
+            heading="Let's work together"
+            headingClassName={styles.heading}
+        >
             <div className={styles.ctaGroup}>
                 <Button href="mailto:hello@lapidist.net">Get in touch</Button>
                 <Button href="/brett-dorrans-cv.pdf" variant="secondary">
