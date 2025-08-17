d7360cd968c911e88961fb4da2463f76a9832561
Brett Dorrans <hello@lapidist.net>
2025-08-17 18:36:48 +0100

    chore: consolidate global style imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Lexend_Deca, Roboto_Mono } from "next/font/google";
-import "@/styles/tokens.scss";
-import "@/styles/globals.scss";
-import "@/styles/typography.scss";
+import "@/styles/index.scss";
 import Header from "@/components/Header/Header";
 import styles from "./layout.module.scss";
 
