bee8172b07eea2dd495779c07346315d7ff5716d
brettdorrans <hello@lapidist.net>
2025-08-25 23:45:42 +0000

    Deploying to gh-pages from @ bylapidist/website@c85195159fde0de3b4f805ae974e2a00210555d9 🚀

---
+++ _next/static/C07XgnR1t3UhzTRaLXiMp/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Farticles\u002F[year]\u002F[slug]","\u002Farticles\u002F[year]\u002F[slug]\u002Fopengraph-image-wdxhve","\u002Farticles\u002F[year]\u002F[slug]\u002Ftwitter-image-wdxhve"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
